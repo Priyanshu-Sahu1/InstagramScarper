ca3baf5ccfe2c119cc3b5717cc1e947ac395916f
Priyanshu-Sahu1 <priyanshu.sahu@iiitg.ac.in>
2023-05-16 11:39:36 +0530

    Update parameter.py

--- Instagram_Hastag_DataScraper/parameter.py
+++ Instagram_Hastag_DataScraper/parameter.py
@@ -2,6 +2,5 @@
 Instagram_username = "Enter_Instagram_Username"
 Instagram_password = "Enter_Instagram_Password"
 #Hashtag from which need to scrape data
-Instagram_Hastag = "Enter_Hashtag"
-#Enter the relative path of your chromedriver
-relative_path = "./chromedriver.exe"
+Instagram_Hashtag = "Enter_Hashtag"
+
